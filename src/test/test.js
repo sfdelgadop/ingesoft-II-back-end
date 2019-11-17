d0936a96e646048ce3911c549271770308ae6709
aramonh <aramonh@unal.edu.co>
2019-11-17 10:20:46 -0500

    1 test not function

---
+++ src/test/test.js
@@ -0,0 +1,17 @@
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+const expect = require('chai').expect;
+chai.use(chaiHttp);
+const url= 'http://localhost:3000';
+
+describe('get all users: ',()=>{
+     it('should get all users', (done) => {
+     chai.request(url)
+     .get('/api/users')
+     .end( function(err,res){
+     console.log(res.body)
+     expect(res).to.have.status(200);
+     done();
+     });
+     });
+    });
